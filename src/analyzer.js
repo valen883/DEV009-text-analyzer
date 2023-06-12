e044d710f521996e0f1c099fdd804c223bc02551
Valentina V <valentinagutierrez882@gmail.com>
2023-06-12 10:38:14 -0500

    funcion recuento de palabras y caracteres

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,11 +1,17 @@
 const analyzer = {  
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //TODO: esta   el parámetro `text` de tipo `string`.
+    const getWordNumber = text.split(" ");
+    return getWordNumber
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    //text.split(''); cadena vacia como separador
+    const characterNumber =  text.length;
+    return characterNumber
   },
   getCharacterCountExcludingSpaces: (text) => {
+    //str.trim()
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
   getAverageWordLength: (text) => {    

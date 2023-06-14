c6c56a0dec0f46bf4ddbc54089fe04fb4249756d
Valentina V <valentinagutierrez882@gmail.com>
2023-06-14 15:30:13 -0500

    textContent

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-//acá debes escuchar eventos del DOM, invocar los métodos del objeto analyzer según 
-//sea necesario y actualizar el resultado en la UI (interfaz de usuario).
 
 function onKeyUp(event) {
   const inputValue = event.target.value;
 
   const wordsCount = analyzer.getWordCount(inputValue)
-  document.querySelector('li[name="wordsCount"] a').innerHTML = wordsCount
+  document.querySelector('li[name="textContent"] a').innerHTML = wordsCount
+
+  const characterCount = analyzer.getCharacterCount(inputValue)
+  document.querySelector('li[name="characterCount"] a').innerHTML = characterCount
 }
 
 const box = document.querySelector('textarea[name="user-input"]');

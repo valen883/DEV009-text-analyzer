e3b076690417e2eefd1869fbe23a89cdcaabddf1
Valentina V <valentinagutierrez882@gmail.com>
2023-06-17 10:11:33 -0500

    correciones html y css

--- src/index.js
+++ src/index.js
@@ -1,35 +1,20 @@
 import analyzer from './analyzer.js';
-
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
-// funcion para tomar el valor recibido en textarea para ser utlizado por el evento
-function onKeyUp(event) {
-  //.target hace referencia al objeto en el cual se lanzo el evento
-  //.value devuelve el valor del objeto
-  const inputValue = event.target.value;
-
-  const wordsCount = analyzer.getWordCount(inputValue)
-  document.querySelector('li[name="wordsCount"] a').innerHTML = wordsCount;
-
-  const characterCount = analyzer.getCharacterCount(inputValue)
-  document.querySelector('li[name="characterCount"] a').innerHTML = characterCount;
-
-  const  characterWtSpaces = analyzer.getCharacterCountExcludingSpaces(inputValue)
-  document.querySelector('li[name="characterWtSpaces"] a').innerHTML = characterWtSpaces
-
-  const numbersCount = analyzer.getNumberCount(inputValue)
-  document.querySelector('li[name="numbersCount"] a').innerHTML = numbersCount
-
-  const Sum = analyzer.getNumberSum(inputValue)
-  document.querySelector('li[name="sum"] a').innerHTML = Sum
-
-  const wordLength = analyzer.getAverageWordLength(inputValue)
-  document.querySelector('li[name="wordLength"] a').innerHTML = wordLength
-  
-}
-// aqui escucha el evemto keyuo por medio del metodo addEventLinstener del primer elemento indicado 
-//por querySelector y la funcion que le indica que debe hacer
 const box = document.querySelector('textarea[name="user-input"]');
-box.addEventListener("keyup", onKeyUp)
-
-
+box.addEventListener("keyup", function () {
+  //.value devuelve el valor del objeto
+  const text = box.value;
+  const charactersCount = analyzer.getCharacterCount(text);
+  document.getElementById("characterCount").innerHTML = charactersCount;
+  const charactsExcludingSpace = analyzer.getCharacterCountExcludingSpaces(text);
+  document.getElementById('characterWtSpaces').innerHTML = charactsExcludingSpace;
+  const wordsNumber = analyzer.getWordCount(text);
+  document.getElementById('wordsCount').innerHTML = wordsNumber;
+  const number = analyzer.getNumberCount(text);
+  document.getElementById('numbersCount').innerHTML = number;
+  const numberSum = analyzer.getNumberSum(text)
+  document.getElementById('sum').innerHTML = numberSum
+  const wordsLength = analyzer.getAverageWordLength(text)
+  document.getElementById('wordLength').innerHTML = wordsLength
+})

25f6c1df906adc8e69c78adad42c4a75e75fbaac
Valentina V <valentinagutierrez882@gmail.com>
2023-06-21 22:27:56 -0500

    update readname

--- src/analyzer.js
+++ src/analyzer.js
@@ -11,16 +11,18 @@ const analyzer = {
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    //La expresión regular [\s\W_] coincide con cualquier carácter que sea un espacio (\s), un carácter no alfanumérico (\W), o un guión bajo (_). L
+    //La expresión regular [\s\W_] coincide con cualquier carácter que sea un espacio (\s), un 
+    //carácter no alfanumérico (\W), o un guión bajo (_). 
     const characExcludingSpacest = text.replace(/[\s\W_]/g, "").length;
     return characExcludingSpacest
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
     const words = text.split(' ');
-    const wordCount = words.length;
+    const wordNumb = words.length;
+    //reduce sumar la longitud de todas las palabras
     const totalLength = words.reduce((acc, word) => acc + word.length, 0);
-    const averageLength = wordCount > 0 ? totalLength / wordCount : 0;
+    const averageLength = wordNumb > 0 ? totalLength / wordNumb : 0;
     return parseFloat(averageLength.toFixed(2));
   },
   getNumberCount: (text) => {
